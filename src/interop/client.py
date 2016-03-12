c101d2a938de707468dfe33f336ab488073b11df
Anass Al <mr@anassinator.com>
2016-03-12 04:45:43 -0500

    Use synchronous HTTP client to rely on ROS threading instead

--- src/interop/client.py
+++ src/interop/client.py
@@ -6,8 +6,7 @@ import urllib
 import serializers
 from tornado.escape import json_decode
 from httpclient_session import Session
-from tornado.gen import coroutine, Return
-from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+from tornado.httpclient import HTTPClient, HTTPRequest
 
 
 class InteroperabilityClient(object):
@@ -31,9 +30,8 @@ class InteroperabilityClient(object):
         """
         self.timeout = timeout
         self.url = url[:-1] if url.endswith('/') else url
-        self.session = Session(AsyncHTTPClient)
+        self.session = Session(HTTPClient)
 
-    @coroutine
     def authenticate(self, username, password):
         """Authenticates with the Interoperability server with given
         credentials.
@@ -45,10 +43,9 @@ class InteroperabilityClient(object):
         Raises:
             HTTPError: On failure or timeout.
         """
-        yield self._post("/api/login",
-                         body={"username": username, "password": password})
+        self._post("/api/login",
+                   body={"username": username, "password": password})
 
-    @coroutine
     def _get(self, uri, **kwargs):
         """Sends GET request to Interoperability server at specified URI.
 
@@ -67,10 +64,9 @@ class InteroperabilityClient(object):
             method="GET",
             request_timeout=self.timeout,
             **kwargs)
-        response = yield self.session.fetch(request)
-        raise Return(response)
+        response = self.session.fetch(request)
+        return response
 
-    @coroutine
     def _put(self, uri, body=None, **kwargs):
         """Sends PUT request to Interoperability server at specified URI.
 
@@ -93,10 +89,9 @@ class InteroperabilityClient(object):
             body=body,
             request_timeout=self.timeout,
             **kwargs)
-        response = yield self.session.fetch(request)
-        raise Return(response)
+        response = self.session.fetch(request)
+        return response
 
-    @coroutine
     def _post(self, uri, body=None, **kwargs):
         """Sends POST request to Interoperability server at specified URI.
 
@@ -119,10 +114,9 @@ class InteroperabilityClient(object):
             body=body,
             request_timeout=self.timeout,
             **kwargs)
-        response = yield self.session.fetch(request)
-        raise Return(response)
+        response = self.session.fetch(request)
+        return response
 
-    @coroutine
     def _delete(self, uri, **kwargs):
         """Sends DELETE request to Interoperability server at specified URI.
 
@@ -141,10 +135,9 @@ class InteroperabilityClient(object):
             method="DELETE",
             request_timeout=self.timeout,
             **kwargs)
-        response = yield self.session.fetch(request)
-        raise Return(response)
+        response = self.session.fetch(request)
+        return response
 
-    @coroutine
     def get_server_info(self):
         """Returns server information.
 
@@ -156,11 +149,10 @@ class InteroperabilityClient(object):
         Raises:
             HTTPError: On failure or timeout.
         """
-        response = yield self._get("/api/server_info")
+        response = self._get("/api/server_info")
         json = json_decode(response.body)
-        raise Return(serializers.ServerInfoDeserializer.from_json(json))
+        return serializers.ServerInfoDeserializer.from_json(json)
 
-    @coroutine
     def get_obstacles(self):
         """Returns obstacles as Markers.
 
@@ -172,11 +164,10 @@ class InteroperabilityClient(object):
         Raises:
             HTTPError: On failure or timeout.
         """
-        response = yield self._get("/api/obstacles")
+        response = self._get("/api/obstacles")
         json = json_decode(response.body)
-        raise Return(serializers.ObstaclesDeserializer.from_json(json))
+        return serializers.ObstaclesDeserializer.from_json(json)
 
-    @coroutine
     def post_telemetry(self, navsat_msg, compass_msg):
         """Uploads telemetry information to Interoperability server.
 
@@ -192,27 +183,33 @@ class InteroperabilityClient(object):
         """
         json = serializers.TelemetrySerializer.from_msg(navsat_msg,
                                                         compass_msg)
-        response = yield self._post("/api/telemetry", body=json)
-        raise Return(response)
+        response = self._post("/api/telemetry", body=json)
+        return response
 
 
 if __name__ == "__main__":
-    from tornado.ioloop import IOLoop
+    import rospy
+    from visualization_msgs.msg import MarkerArray
+
+    # Initialize node.
+    rospy.init_node("test_interop_client")
+
+    # Setup publishers.
+    rate = rospy.Rate(24)
+    moving = rospy.Publisher("/moving_obstacles", MarkerArray, queue_size=1)
+    stationary = rospy.Publisher("/stationary_obstacles",
+                                 MarkerArray, queue_size=1)
 
     # Initialize client.
     client = InteroperabilityClient("http://localhost:80")
 
-    @coroutine
-    def authenticate():
-        """Authenticate with server."""
-        yield client.authenticate("testadmin", "testpass")
+    # Test out authentication and basic request.
+    client.authenticate("testadmin", "testpass")
+    print(client.get_server_info())
 
-    @coroutine
-    def print_server_info():
-        """Print out server information."""
-        r = yield client.get_server_info()
-        print(r)
-
-    # Test out authentication and basic request synchronously.
-    IOLoop.current().run_sync(authenticate)
-    IOLoop.current().run_sync(print_server_info)
+    # Publish obstacles.
+    while not rospy.is_shutdown():
+        moving_obj, stationary_obj = client.get_obstacles()
+        moving.publish(moving_obj)
+        stationary.publish(stationary_obj)
+        rate.sleep()
